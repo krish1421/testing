e61a2a345e10630b80126ce2637b9db14ae600ee
krishna <tramkrishna9998@gmail.com>
2022-10-10 18:48:14 +0530

    setting

--- hello_django/hello_django/settings.py
+++ hello_django/hello_django/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 # import dj_database_url
 from pathlib import Path
 import os
+import django_on_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -140,4 +141,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# django_on_heroku.settings(locals())
+django_on_heroku.settings(locals())
